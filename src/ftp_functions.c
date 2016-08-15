f77ba94888ac5b7cc273c6cac27e37f5985c481a
Aakash Ravi <aakash_ravi@hotmail.com>
2016-08-15 18:30:22 +0200

    Debugging

--- src/ftp_functions.c
+++ src/ftp_functions.c
@@ -198,6 +198,7 @@ ftp_thread(void * args) {
 			char * command = strtok(buf_ptr, " ");
 
 			printf("This is the command: %s\n", command);
+			fflush(stdout);
 
 			print_debug("\nClient input: ");
 			print_debug(command);

c064ca49b840c7f1aa3b3efee487a7f5ceeedc1a
Aakash Ravi <aakash_ravi@hotmail.com>
2016-09-01 01:11:36 +0200

    Testing free error

--- src/ftp_functions.c
+++ src/ftp_functions.c
@@ -452,8 +452,8 @@ EPSV_HANDLER(client_context_t * current_context) {
 	current_context->active_flag = 0;
 	free(full_client_message);
 	full_client_message = NULL;
-	free(local_ip_address);
-	local_ip_address = NULL;
+	// free(local_ip_address);
+	// local_ip_address = NULL;
 }
 
 // Handler function for the CWD FTP command

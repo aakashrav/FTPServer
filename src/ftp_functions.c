063d6fd6b3284eb5532b87160798404f673e1cf9
Aakash Ravi <aakash_ravi@hotmail.com>
2016-09-01 01:17:20 +0200

    Testing free error

--- src/ftp_functions.c
+++ src/ftp_functions.c
@@ -450,8 +450,8 @@ EPSV_HANDLER(client_context_t * current_context) {
 
 	// We switch active mode off and deallocate resources
 	current_context->active_flag = 0;
-	// free(full_client_message);
-	// full_client_message = NULL;
+	free(full_client_message);
+	full_client_message = NULL;
 	free(local_ip_address);
 	local_ip_address = NULL;
 }
